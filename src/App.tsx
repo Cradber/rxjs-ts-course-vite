03edf96e1fa62604c6396355a4f8f29effcdbabb
Yankie <yankielarapacheco@yahoo.com>
2022-08-26 17:41:12 +0200

    Vite project with JSX, TS with RxJS for reactive programming course

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {h} from "preact";
 import typescriptLogo from "./assets/typescript.svg";
 import vite from "./assets/vite.svg";
 import jsx from "./assets/jsx.svg";
+import rx from "./assets/rx.png";
 
 import {LogoLink} from "./LogoLink";
 
@@ -11,6 +12,8 @@ export const App = () => (
         <LogoLink href="https://vitejs.dev" src={vite} className="logo" alt="TypeScript logo" />
         <LogoLink href="https://www.typescriptlang.org/" src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
         <LogoLink href="https://reactjs.org/docs/introducing-jsx.html" src={jsx} className="logo" alt="JSX logo" />
-        <h1>Vite + TypeScript + JSX</h1>
+        <LogoLink href="https://reactivex.io" src={rx} className="logo" alt="RxJS logo" />
+        <h1>Vite + TypeScript + JSX + RxJS</h1>
+        <span>RxJS little course</span>
     </div>
 )
