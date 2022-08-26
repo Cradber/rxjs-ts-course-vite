91e0cf0e9efb148133fb4c3ef976fe1bd1e5e150
Yankie <yankielarapacheco@yahoo.com>
2022-08-26 18:00:23 +0200

    Refactoring project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import {h} from "preact";
+import {LogoLink} from "./components";
+import {Log} from "./helpers/log";
 
-import typescriptLogo from "./assets/typescript.svg";
-import vite from "./assets/vite.svg";
-import jsx from "./assets/jsx.svg";
-import rx from "./assets/rx.png";
+import {jsx, rx, typescriptLogo, vite} from "./assets";
 
-import {LogoLink} from "./LogoLink";
+export const App = () => {
+    Log()
 
-export const App = () => (
-    <div>
-        <LogoLink href="https://vitejs.dev" src={vite} className="logo" alt="TypeScript logo" />
-        <LogoLink href="https://www.typescriptlang.org/" src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
-        <LogoLink href="https://reactjs.org/docs/introducing-jsx.html" src={jsx} className="logo" alt="JSX logo" />
-        <LogoLink href="https://reactivex.io" src={rx} className="logo" alt="RxJS logo" />
-        <h1>Vite + TypeScript + JSX + RxJS</h1>
-        <span>RxJS little course</span>
-    </div>
-)
+    return (
+        <div>
+                <LogoLink href="https://vitejs.dev" src={vite} className="logo" alt="TypeScript logo" />
+                <LogoLink href="https://www.typescriptlang.org/" src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
+                <LogoLink href="https://reactjs.org/docs/introducing-jsx.html" src={jsx} className="logo" alt="JSX logo" />
+                <LogoLink href="https://reactivex.io" src={rx} className="logo" alt="RxJS logo" />
+                <h1>Vite + TypeScript + JSX + RxJS</h1>
+                <span>RxJS little course</span>
+        </div>
+    )
+}
---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import typescriptLogo from "./typescript.svg";
+import vite from "./vite.svg";
+import jsx from "./jsx.svg";
+import rx from "./rx.png";
+
+export {
+    typescriptLogo,
+    vite,
+    jsx,
+    rx
+}
